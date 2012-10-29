38e95ee1a41b498b93abc46de421213aea176c2c
diki <musdikici@gmail.com>
2012-10-29 03:51:20 +0200

    resolved landing page

--- lib/templates/home.php
+++ lib/templates/home.php
@@ -1,14 +1,9 @@
-{% extends "master.php" %}
+{% extends "landing.php" %}
 
 {% block title %} {{ title }} {% endblock %}
 
 {% block content %}
     
-    <script src="/js/lib/jquery.js" type="text/javascript"></script>
-
-    <div style="z-index: 0;">
-        {% include 'rotating_slider.php' %}
-    </div>
     <div class="main-container">
 <!--         <h1 class="main-logo-line">
             <span>
