92eaa9d731448807193a39a83b69c05d1be16bb4
diki <musdikici@gmail.com>
2012-10-30 02:55:57 +0200

    changes

--- lib/templates/landing.php
+++ lib/templates/landing.php
@@ -13,7 +13,7 @@
         
         {% include 'header2.php' %}
 
-        <script src="/js/lib/jquery.js" type="text/javascript"></script>
+        <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js" type="text/javascript"></script>
 
         <div style="z-index: 0;">
             {% include 'rotating_slider.php' %}
